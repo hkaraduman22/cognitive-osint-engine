8d1ebd7c9b3c0afd4cf28b51d499927acd269abc
USR <hissenem07@gmail.com>
2026-07-06 09:01:54 +0300

    feat(ui): 7. Gun Code Freeze - Analitik grafikler, asenkron tarama, tam grid entegrasyonu tamamlandi

--- autonomous-osint-agent/uMain.dfm
+++ autonomous-osint-agent/uMain.dfm
@@ -1,9 +1,9 @@
 object frmMain: TfrmMain
   Left = 0
   Top = 0
-  Caption = 'Akilli OSINT Firma Rehberi'
-  ClientHeight = 600
-  ClientWidth = 800
+  Caption = 'Ak'#305'll'#305' OSINT Firma Rehberi - Code Freeze (v1.0)'
+  ClientHeight = 700
+  ClientWidth = 1000
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -11,6 +11,171 @@ object frmMain: TfrmMain
   Font.Name = 'Segoe UI'
   Font.Style = []
   Position = poScreenCenter
+  OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 15
+  object edtSehir: TEdit
+    Left = 16
+    Top = 16
+    Width = 150
+    Height = 23
+    TabOrder = 0
+    TextHint = 'Sehir (Orn: Ankara)'
+  end
+  object cmbSektor: TComboBox
+    Left = 180
+    Top = 16
+    Width = 150
+    Height = 23
+    TabOrder = 1
+    Text = 'Sektor Seciniz'
+  end
+  object cmbCalisanSayisi: TComboBox
+    Left = 345
+    Top = 16
+    Width = 120
+    Height = 23
+    TabOrder = 2
+    Text = 'Calisan Sayisi'
+  end
+  object edtMinPuan: TEdit
+    Left = 480
+    Top = 16
+    Width = 100
+    Height = 23
+    TabOrder = 3
+    TextHint = 'Min Puan (85)'
+  end
+  object chkSadeceYatirimAlanlar: TCheckBox
+    Left = 600
+    Top = 19
+    Width = 120
+    Height = 17
+    Caption = 'Yatirim Alanlar'
+    TabOrder = 4
+  end
+  object btnAra: TButton
+    Left = 730
+    Top = 15
+    Width = 100
+    Height = 25
+    Caption = 'Vitrin Ara'
+    TabOrder = 5
+    OnClick = btnAraClick
+  end
+  object btnTaramaBaslat: TButton
+    Left = 840
+    Top = 15
+    Width = 140
+    Height = 25
+    Caption = 'OSINT Taramasi Baslat'
+    TabOrder = 6
+    OnClick = btnTaramaBaslatClick
+  end
+  object btnAnalizGetir: TButton
+    Left = 16
+    Top = 45
+    Width = 964
+    Height = 25
+    Caption = 'Sektorel Dagilim Analizini Getir (Grafik)'
+    TabOrder = 7
+    OnClick = btnAnalizGetirClick
+  end
+  object dbgSonuclar: TDBGrid
+    Left = 16
+    Top = 80
+    Width = 964
+    Height = 350
+    DataSource = dsSonuclar
+    Options = [dgTitles, dgIndicator, dgColumnResize, dgColLines, dgRowLines, dgTabs, dgRowSelect, dgConfirmDelete, dgCancelOnExit, dgTitleClick, dgTitleHotTrack]
+    TabOrder = 8
+    TitleFont.Charset = DEFAULT_CHARSET
+    TitleFont.Color = clWindowText
+    TitleFont.Height = -12
+    TitleFont.Name = 'Segoe UI'
+    TitleFont.Style = []
+    OnDblClick = dbgSonuclarDblClick
+  end
+  object mmoLoglar: TMemo
+    Left = 16
+    Top = 440
+    Width = 473
+    Height = 240
+    Color = clBlack
+    Font.Charset = ANSI_CHARSET
+    Font.Color = clLime
+    Font.Height = -12
+    Font.Name = 'Consolas'
+    Font.Style = []
+    ParentFont = False
+    ReadOnly = True
+    ScrollBars = ssVertical
+    TabOrder = 9
+  end
+  object chtSektor: TChart
+    Left = 507
+    Top = 440
+    Width = 473
+    Height = 240
+    Title.Text.Strings = (
+      'Sektor Dagilimi')
+    View3DOptions.Elevation = 315
+    View3DOptions.Orthogonal = False
+    View3DOptions.Perspective = 0
+    View3DOptions.Rotation = 360
+    TabOrder = 10
+    DefaultCanvas = 'TGDIPlusCanvas'
+    ColorPaletteIndex = 13
+  end
+  object RestClient: TRESTClient
+    BaseURL = 'http://localhost:8000/api/v1'
+    Params = <>
+    Left = 32
+    Top = 120
+  end
+  object RestRequest: TRESTRequest
+    Client = RestClient
+    Params = <>
+    Response = RestResponse
+    Left = 104
+    Top = 120
+  end
+  object RestResponse: TRESTResponse
+    Left = 184
+    Top = 120
+  end
+  object RestAdapter: TRESTResponseDataSetAdapter
+    Dataset = MemTableSonuclar
+    FieldDefs = <>
+    Response = RestResponse
+    Left = 272
+    Top = 120
+  end
+  object MemTableSonuclar: TFDMemTable
+    FetchOptions.AssignedValues = [evMode]
+    FetchOptions.Mode = fmAll
+    ResourceOptions.AssignedValues = [rvSilentMode]
+    ResourceOptions.SilentMode = True
+    UpdateOptions.AssignedValues = [uvCheckRequired, uvAutoCommitUpdates]
+    UpdateOptions.CheckRequired = False
+    UpdateOptions.AutoCommitUpdates = True
+    Left = 376
+    Top = 120
+  end
+  object dsSonuclar: TDataSource
+    DataSet = MemTableSonuclar
+    Left = 472
+    Top = 120
+  end
+  object RestReqStats: TRESTRequest
+    Client = RestClient
+    Params = <>
+    Response = RestResStats
+    Left = 104
+    Top = 184
+  end
+  object RestResStats: TRESTResponse
+    Left = 184
+    Top = 184
+  end
 end
--- autonomous-osint-agent/uMain.pas
+++ autonomous-osint-agent/uMain.pas
@@ -9,15 +9,24 @@ uses
   FireDAC.Phys.Intf, FireDAC.DApt.Intf, Data.DB, FireDAC.Comp.DataSet,
   FireDAC.Comp.Client, REST.Response.Adapter, REST.Client, Data.Bind.Components,
   Data.Bind.ObjectScope, Vcl.Grids, Vcl.DBGrids, Vcl.StdCtrls,
-  System.Threading; // Asenkron işlemler için 2. gün eklemiştik
+  System.Threading, System.JSON, VCLTee.TeEngine, VCLTee.Series, Vcl.ExtCtrls,
+  VCLTee.TeeProcs, VCLTee.Chart;
 
 type
   TfrmMain = class(TForm)
-    // --- 1. Gün Bileşenleri ---
     edtSehir: TEdit;
     cmbSektor: TComboBox;
+    cmbCalisanSayisi: TComboBox;
+    edtMinPuan: TEdit;
+    chkSadeceYatirimAlanlar: TCheckBox;
     btnAra: TButton;
+    btnTaramaBaslat: TButton;
+    btnAnalizGetir: TButton;
     dbgSonuclar: TDBGrid;
+    mmoLoglar: TMemo;
+    chtSektor: TChart;
+
+    // API Bilesenleri
     RestClient: TRESTClient;
     RestRequest: TRESTRequest;
     RestResponse: TRESTResponse;
@@ -25,24 +34,19 @@ type
     MemTableSonuclar: TFDMemTable;
     dsSonuclar: TDataSource;
 
-    // --- 2. Gün Bileşenleri ---
-    btnTaramaBaslat: TButton;
-    RestRequestPOST: TRESTRequest;
-    RestResponsePOST: TRESTResponse;
-
-    // --- 3. Gün Bileşenleri (Gelişmiş Filtreler) ---
-    cmbCalisanSayisi: TComboBox;
-    edtMinPuan: TEdit;
-    chkSadeceYatirimAlanlar: TCheckBox;
+    // Analitik icin ayri istek bileseni
+    RestReqStats: TRESTRequest;
+    RestResStats: TRESTResponse;
 
     procedure btnAraClick(Sender: TObject);
-    procedure FormCreate(Sender: TObject);
     procedure btnTaramaBaslatClick(Sender: TObject);
+    procedure btnAnalizGetirClick(Sender: TObject);
+    procedure dbgSonuclarDblClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
-    { Private declarations }
-    procedure GelismisAramaYap; // Eski APIyeBaglanVeAra'nın yerine bu geldi
+    procedure GelismisAramaYap;
+    procedure LogYaz(AMesaj: string);
   public
-    { Public declarations }
   end;
 
 var
@@ -52,42 +56,47 @@ implementation
 
 {$R *.dfm}
 
+procedure TfrmMain.LogYaz(AMesaj: string);
+begin
+  mmoLoglar.Lines.Add(FormatDateTime('hh:nn:ss', Now) + ' [SYS] > ' + AMesaj);
+end;
+
 procedure TfrmMain.FormCreate(Sender: TObject);
 begin
-  // Uygulama açıldığında varsayılan ayarlar
-  edtSehir.TextHint := 'Şehir Giriniz (Örn: Ankara)';
-  edtMinPuan.TextHint := 'Min Yapay Zeka Puanı (Örn: 85)';
-
   if cmbSektor.Items.Count = 0 then
+  begin
     cmbSektor.Items.Add('Tümü');
+    cmbSektor.Items.Add('Yazılım');
+    cmbSektor.Items.Add('Otomotiv');
+    cmbSektor.Items.Add('Tekstil');
+  end;
   cmbSektor.ItemIndex := 0;
 
   if cmbCalisanSayisi.Items.Count = 0 then
+  begin
     cmbCalisanSayisi.Items.Add('Tümü');
+    cmbCalisanSayisi.Items.Add('1-50');
+    cmbCalisanSayisi.Items.Add('50-250');
+    cmbCalisanSayisi.Items.Add('250+');
+  end;
   cmbCalisanSayisi.ItemIndex := 0;
+
+  LogYaz('Sistem baslatildi. Tum moduller (Code Freeze) yayinda.');
 end;
 
-// --- 3. GÜN: GET İSTEĞİ (QUERY BUILDER) ---
+// 5. GUN: VİTRİN VE FİLTRELEME
 procedure TfrmMain.GelismisAramaYap;
 begin
+  LogYaz('Filtrelere gore veritabaninda arama yapiliyor...');
   RestRequest.Params.Clear;
   RestRequest.Method := rmGET;
   RestRequest.Resource := 'companies';
 
-  if Trim(edtSehir.Text) <> '' then
-    RestRequest.AddParameter('sehir', Trim(edtSehir.Text), pkGETrsUnsaved);
-
-  if (cmbSektor.ItemIndex > -1) and (cmbSektor.Text <> 'Tümü') then
-    RestRequest.AddParameter('sektor', cmbSektor.Text, pkGETrsUnsaved);
-
-  if (cmbCalisanSayisi.ItemIndex > -1) and (cmbCalisanSayisi.Text <> 'Tümü') then
-    RestRequest.AddParameter('calisan_araligi', cmbCalisanSayisi.Text, pkGETrsUnsaved);
-
-  if Trim(edtMinPuan.Text) <> '' then
-    RestRequest.AddParameter('min_puan', Trim(edtMinPuan.Text), pkGETrsUnsaved);
-
-  if chkSadeceYatirimAlanlar.Checked then
-    RestRequest.AddParameter('yatirim_aldi_mi', 'true', pkGETrsUnsaved);
+  if Trim(edtSehir.Text) <> '' then RestRequest.AddParameter('sehir', Trim(edtSehir.Text), pkGETrsUnsaved);
+  if (cmbSektor.ItemIndex > 0) then RestRequest.AddParameter('sektor', cmbSektor.Text, pkGETrsUnsaved);
+  if (cmbCalisanSayisi.ItemIndex > 0) then RestRequest.AddParameter('calisan_araligi', cmbCalisanSayisi.Text, pkGETrsUnsaved);
+  if Trim(edtMinPuan.Text) <> '' then RestRequest.AddParameter('min_puan', Trim(edtMinPuan.Text), pkGETrsUnsaved);
+  if chkSadeceYatirimAlanlar.Checked then RestRequest.AddParameter('yatirim_aldi_mi', 'true', pkGETrsUnsaved);
 
   try
     RestRequest.Execute;
@@ -95,12 +104,12 @@ begin
     begin
       MemTableSonuclar.Close;
       MemTableSonuclar.Open;
+      LogYaz('Basarili: ' + IntToStr(MemTableSonuclar.RecordCount) + ' kayit bulundu.');
     end
     else
-      ShowMessage('Arama başarısız oldu. API Yanıt Kodu: ' + RestResponse.StatusCode.ToString);
+      LogYaz('Hata: API Kodu ' + RestResponse.StatusCode.ToString);
   except
-    on E: Exception do
-      ShowMessage('API bağlantı hatası: ' + E.Message);
+    on E: Exception do LogYaz('Baglanti Hatasi: ' + E.Message);
   end;
 end;
 
@@ -109,25 +118,26 @@ begin
   GelismisAramaYap;
 end;
 
-// --- 2. GÜN: POST İSTEĞİ (ASENKRON TARAMA) ---
+// 4. GUN: OSINT BOTUNU ASENKRON TETIKLEME
 procedure TfrmMain.btnTaramaBaslatClick(Sender: TObject);
 begin
-  ShowMessage('OSINT Taraması arka planda başlatıldı. Lütfen bekleyin...');
+  LogYaz('OSINT Taramasi baslatiliyor. Bot ve LLM isleme alindi...');
   btnTaramaBaslat.Enabled := False;
 
   TTask.Run(procedure
   begin
-    RestRequestPOST.Method := rmPOST;
-    RestRequestPOST.Resource := 'companies/scan';
+    RestReqStats.Method := rmPOST;
+    RestReqStats.Resource := 'companies/scan';
 
     try
-      RestRequestPOST.Execute;
+      RestReqStats.Execute;
       TThread.Queue(nil, procedure
       begin
-        if RestResponsePOST.StatusCode = 200 then
-          ShowMessage('Tarama başarıyla tamamlandı!')
+        if RestResStats.StatusCode = 200 then
+          LogYaz('MUKEMMEL: Bot verileri cekti, LLM eledi ve DB''ye kaydedildi!')
         else
-          ShowMessage('Tarama hata kodu döndü: ' + RestResponsePOST.StatusCode.ToString);
+          LogYaz('Hata: Tarama sirasinda sorun olustu. Kod: ' + RestResStats.StatusCode.ToString);
+
         btnTaramaBaslat.Enabled := True;
       end);
     except
@@ -135,12 +145,72 @@ begin
       begin
         TThread.Queue(nil, procedure
         begin
-          ShowMessage('API bağlantı hatası: ' + E.Message);
+          LogYaz('Baglanti Hatasi (Bot Cagrilarinda): ' + E.Message);
           btnTaramaBaslat.Enabled := True;
         end);
       end;
     end;
   end);
+end;
+
+// 6. GUN: ANALITIK VE GRAFIKLER (TChart)
+procedure TfrmMain.btnAnalizGetirClick(Sender: TObject);
+var
+  JSONObj: TJSONObject;
+  JSONPair: TJSONPair;
+  PieSeries: TPieSeries;
+  i: Integer;
+begin
+  LogYaz('Analitik endpoint''inden sektorel dagilim verisi cekiliyor...');
+  RestReqStats.Method := rmGET;
+  RestReqStats.Resource := 'stats/industry-distribution';
+
+  try
+    RestReqStats.Execute;
+    if RestResStats.StatusCode = 200 then
+    begin
+      // Eski grafigi temizle
+      chtSektor.RemoveAllSeries;
+      PieSeries := TPieSeries.Create(chtSektor);
+      chtSektor.AddSeries(PieSeries);
+
+      // JSON Pars Etme
+      JSONObj := TJSONObject.ParseJSONValue(RestResStats.Content) as TJSONObject;
+      if Assigned(JSONObj) then
+      begin
+        try
+          for i := 0 to JSONObj.Count - 1 do
+          begin
+            JSONPair := JSONObj.Pairs[i];
+            PieSeries.AddPie(StrToIntDef(JSONPair.JsonValue.Value, 0), JSONPair.JsonString.Value, clTeeColor);
+          end;
+          LogYaz('Grafik basariyla cizildi.');
+        finally
+          JSONObj.Free;
+        end;
+      end;
+    end
+    else
+      LogYaz('Analitik verisi alinamadi.');
+  except
+    on E: Exception do LogYaz('Analitik Hatasi: ' + E.Message);
+  end;
+end;
+
+// 5. GUN: DETAY EKRANI
+procedure TfrmMain.dbgSonuclarDblClick(Sender: TObject);
+var
+  Detay: string;
+begin
+  if MemTableSonuclar.IsEmpty then Exit;
+
+  // JSON'dan gelen field isimlerine gore (Swagger'a uygun varsayilmistir)
+  Detay := 'Firma: ' + MemTableSonuclar.FieldByName('firma_adi').AsString + #13#10 +
+           'Sektor: ' + MemTableSonuclar.FieldByName('sektor').AsString + #13#10 +
+           'Guven Puani: ' + MemTableSonuclar.FieldByName('guven_puani').AsString;
+
+  ShowMessage(Detay);
+  LogYaz(MemTableSonuclar.FieldByName('firma_adi').AsString + ' detaylari incelendi.');
 end;
 
 end.
